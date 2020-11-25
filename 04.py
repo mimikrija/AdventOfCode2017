fdfa983725f5f2e3b446906d2d4d7a948cc4bd2a
Maja Gacesa <maja.gacesa@gmail.com>
2020-11-25 20:48:16 +0100

    [day04] move ifs to the function definition

--- 04.py
+++ 04.py
@@ -1,22 +1,23 @@
-def has_unique(words):
-    return len(set(words)) == len(words)
+def unique(words):
+    if len(set(words)) == len(words):
+        return 1
+    else:
+        return 0
 
 def no_anagrams(words):
     temp = []
     for word in words:
         anagram = "".join(sorted(word))
         temp.append(anagram)
-    return has_unique(temp)
+    return unique(temp)
 
 solution_1 = 0
 solution_2 = 0
 
 for line in open('inputs/04','r'):
     pass_phrase = line.split()
-    if has_unique(pass_phrase):
-        solution_1 += 1
-        if no_anagrams(pass_phrase):
-            solution_2 += 1
+    solution_1 += unique(pass_phrase)
+    solution_2 += no_anagrams(pass_phrase)
 
 print("There are ", solution_1, "unique pass phrases!")
 # There are  451 unique pass phrases!
