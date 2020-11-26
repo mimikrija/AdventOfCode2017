b4ff08cae738c4005adcb7e46006b53cb556309d
Maja Gacesa <maja.gacesa@gmail.com>
2020-11-26 20:28:13 +0100

    [day06] part 2 - revrite as a function

--- 06.py
+++ 06.py
@@ -6,7 +6,20 @@ def list_to_string(memory):
     for n in memory:
         memory_string += str(n)
     return memory_string
-        
+
+def do_the_shuffle(memory):
+    chosen = max(memory) # the memory block that gets redistributed
+    positions = [(memory.index(chosen)+ 1 + i) % size for i in range(size)]
+
+    memory[memory.index(chosen)] = 0 # reset chosen memory slot
+
+    i = 0
+    for n in positions:
+        memory[n] += chosen // size
+        if i < chosen%size:
+            memory[n] += 1
+        i += 1
+    return(memory)
 
 with open('./inputs/06', 'r') as infile:
     memory = infile.readline().split()
@@ -14,27 +27,28 @@ with open('./inputs/06', 'r') as infile:
 for n in memory:
     memory[memory.index(n)] = int(n)
 
+size = len(memory)
+
+# part 1
 instructions=[list_to_string(memory)]
-
-solution_1 = 0 # number of cycles
-
+solution_1 = 0
 
 while is_unique(instructions):
-    chosen = max(memory)
-
-    positions = [(memory.index(chosen)+ 1 + i) % 16 for i in range(16)]
-    memory[memory.index(chosen)] = 0 # reset chosen memory slot
-
+    instructions.append(list_to_string(do_the_shuffle(memory)))
     solution_1 += 1
 
-    i = 0
-    for n in positions:
-        memory[n] += chosen // 16
-        if i < chosen%16:
-            memory[n] += 1
-        i += 1
+# part 2
+memory_2 = memory.copy()
+instructions_2 = []
+solution_2 = 0
 
-    instructions.append(list_to_string(memory))
+while memory not in instructions_2:
+    instructions_2.append(do_the_shuffle(memory_2))
+    solution_2 += 1
+
 
 print("It takes ", solution_1, "cycles to reach the same memory configuration!")
 # It takes  11137 cycles to reach the same memory configuration!
+
+print("It takes ", solution_2, "more cycles to reach it again!")
+# It takes  1037 more cycles to reach it again!
