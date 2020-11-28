fca6ee3c5409a63c368c2e7ff4550190f7feb73d
Maja Gacesa <maja.gacesa@gmail.com>
2020-11-28 16:40:58 +0100

    [day07] part 2, ugh
    I guess a better way would be to properly implement a tree (trie) or
    just use a module for it. The way I have implemented it here is that I
    only have a dictionary which tells me for each node what is the nodes
    weight and who are the nodes children. Nothing is nested, so going through
    the collection, finding neighbors (same-level nodes) is cumbersome.
    
    Also, readability sucks. `dict[node][0]` is the node weight, `dict[node][1]`
    is the list of node children. So the value of every key in the dict is a
    list which contains an integer and a list of strings.
    
    Anyway, I am looking forward to rewriting this and learning something.

--- 07.py
+++ 07.py
@@ -1,20 +1,84 @@
 import collections
 
+def is_same(configuration):
+    return len(set(configuration)) == 1
+
+def who_is_unique(input_collection):
+    count_stuff = collections.Counter(input_collection)
+    for thing in count_stuff:
+        if count_stuff[thing] == 1:
+            return(thing)
+    return None
+
+def who_is_common(input_collection):
+    count_stuff = collections.Counter(input_collection)
+    for thing in count_stuff:
+        if count_stuff[thing] > 1:
+            return(thing)
+    return None
+
+def calculate_weight(node):
+    weight = program_dict[node][0]
+    if program_dict[node][1] != []:
+        for child in program_dict[node][1]:
+            weight += calculate_weight(child)
+    return weight
+
+def who_is_unbalanced(node):
+    weights = {}
+    if program_dict[node][1] == []:
+        return None
+    else:
+        for child in program_dict[node][1]:
+            weights[child] = calculate_weight(child)
+        if who_is_unique(weights.values()) == None:
+            return None
+        else:
+            who = list(weights.values()).index(who_is_unique(weights.values()))
+            return list(weights.keys())[who]
+
+
 all_programs = []
+all_inputs = []
 
 with open('./inputs/07', 'r') as infile:
     for line in infile.readlines():
         for word in line.split():
             if word != '->' and '(' not in word:
                 all_programs.append(word.replace(',',''))
+        all_inputs.append(line)
 
+program_dict = {}
 
-count_programs = collections.Counter(all_programs)
+# parse input for part 2
+for line in all_inputs:
+    s = line.index('(')
+    e = line.index(')')
+    line = line.strip()
+    node = line[0:s-1]
+    weight = line[s+1:e]
+    if '->' in line:
+        children = (line.split('-> ')[1]).split(', ')
+    else:
+        children = []
+    program_dict[node] = [int(weight),children]
 
-for word in count_programs:
-    if count_programs[word] == 1:
-         solution_1 = word
-         break
+solution_1 = who_is_unique(all_programs)
+
+# part 2
+current = solution_1
+while True:
+    if who_is_unbalanced(current) == None:
+        break
+    previous = current
+    current = who_is_unbalanced(current)
+
+neighbor_weights = [ calculate_weight(child) for child in program_dict[previous][1] ]
+
+solution_2 = program_dict[current][0] + who_is_common(neighbor_weights) - who_is_unique(neighbor_weights)
 
 print(f"The program at the bottom is: {solution_1}!")
 # The program at the bottom is: hlhomy!
+
+print(f"The weight of the unbalanced program should be: {solution_2}!")
+# The weight of the unbalanced program should be: 1505!
