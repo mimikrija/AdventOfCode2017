9ab0329fdc2af4f6047189505f4905045f12d631
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-11 20:19:58 +0100

    [day09] move all into a function

--- python/09.py
+++ python/09.py
@@ -4,35 +4,37 @@ from santas_little_helpers import *
 from collections import deque
 
 
+def process_stream(stream):
+
+    braces = deque()
+    groups = 0
+    garbage = 0
+
+    while stream:
+        current = stream.popleft()
+        if current == '{':
+            braces.append('{')
+        if current == '}':
+            # close group
+            groups += len(braces)
+            braces.pop()
+        # garbage
+        if current == '<':
+            while True:
+                current = stream.popleft()
+                if current == '!':
+                    stream.popleft()
+                    continue
+                if current == '>':
+                    break
+                else:
+                    garbage += 1
+
+    return groups, garbage
+
+
 stream = deque(get_input('inputs/09')[0])
-
-
-braces = deque()
-groups = 0
-garbage = 0
-
-while stream:
-    current = stream.popleft()
-    if current == '{':
-        braces.append('{')
-    if current == '}':
-        # close group
-        groups += len(braces)
-        braces.pop()
-    # garbage
-    if current == '<':
-        while True:
-            current = stream.popleft()
-            if current == '!':
-                stream.popleft()
-                continue
-            if current == '>':
-                break
-            else:
-                garbage += 1
-
-
-print_solutions(groups, garbage)
+print_solutions(*process_stream(stream))
 
 # Part 1 solution is: 10616
 # Part 2 solution is: 5101
