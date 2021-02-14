910d4cccfcbe9bf482ce97c03d92f6c4afdaf168
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-14 20:43:24 +0100

    [day11] part 2 - use hex grid as key for max he he

--- python/11.py
+++ python/11.py
@@ -1,7 +1,7 @@
 # Day 11: Hex Ed
 
 from santas_little_helpers import *
-import re
+
 
 HEX_DIRECTIONS = {
      'n': (0, 1, -1),
@@ -12,12 +12,28 @@ HEX_DIRECTIONS = {
     'nw': (-1, 1, 0)
 }
 
+def hex_grid_distance(in_hex):
+    " Red Blob Manhattan distance of `in_hex` from (0, 0, 0) using cube coords"
+    return sum(abs(coord) for coord in in_hex) // 2
 
 def hex_add(in_hex_a, in_hex_b):
     return tuple(coordinate_a + coordinate_b for coordinate_a, coordinate_b in zip(in_hex_a, in_hex_b))
 
-def hex_neighbor(in_hex, direction):
-    return hex_add(in_hex, HEX_DIRECTIONS[direction])
+def visit_all(in_directions):
+    start = (0, 0, 0)
+    current = start
+    all_coords = set()
+    for direction in directions:
+        current = hex_add(current, HEX_DIRECTIONS[direction])
+        all_coords.add(current)
 
-def tile_neighbors(in_hex):
-    return {hex_neighbor(in_hex, direction) for direction in HEX_DIRECTIONS}
+    return map(hex_grid_distance, (current, max(all_coords, key=hex_grid_distance)))
+
+
+directions = get_input('inputs/11',',')
+
+part_1, part_2 = visit_all(directions)
+
+print_solutions(part_1, part_2)
+# Part 1 solution is: 784
+# Part 2 solution is: 1558
