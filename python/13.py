ad3886a41eeda52f62bdb92a7f5257969dd0e255
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-17 19:53:47 +0100

    [day13] pt1 find all times when scanner is at zero
    the scanner will be at position zero in multiples of (size-1)*2, and
    the layer depth corresponds to the time it takes to reach that layer.
    
    so we only need to count those layers for which time is equivalent to
    the multiple of zero position times.

--- python/13.py
+++ python/13.py
@@ -2,5 +2,15 @@
 
 from santas_little_helpers import *
 
+def scanner_at_zero_position(time, size):
+    return time % ((size-1)*2) == 0
+
+
 depth_and_range = {layer: size for layer, size in (map(int, line.split(': ')) for line in get_input('inputs/13'))}
 
+# part 1
+severity = sum(time*size for time, size in depth_and_range.items() if scanner_at_zero_position(time, size))
+
+
+print_solutions(severity)
+# Part 1 solution is: 2384
