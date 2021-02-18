e41f790d118cfbb32e4d07cf97f56a2cd8c2b550
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-18 19:45:51 +0100

    [day16] part 1

--- python/16.py
+++ python/16.py
@@ -2,4 +2,36 @@
 
 from santas_little_helpers import *
 
-dance_instructions = get_input('inputs/16',',')
+
+def spin(in_list, count):
+    return in_list[-count:] + in_list[:-count]
+
+def exchange(in_list, A, B):
+    first, second = sorted((A, B))
+    return in_list[:first] + [in_list[second]] + in_list[first+1:second] + [in_list[first]] + in_list[second+1:]
+
+def partner(in_list, name_A, name_B):
+    return exchange(in_list, in_list.index(ord(name_A)), in_list.index(ord(name_B)))
+
+def dance(in_instructions, sequence):
+    for args in in_instructions:
+        try:
+            arguments = list(map(int, args))
+            if len(arguments) == 1:
+                sequence = spin(sequence, *arguments)
+            else:
+                sequence = exchange(sequence, *arguments)
+        except:
+            sequence = partner(sequence, *args)
+    return sequence
+
+
+dance_instructions = (instruction[1:].split('/') for instruction in get_input('inputs/16',','))
+sequence = list(range(ord('a'), ord('p')+1))
+
+
+sequence = dance(dance_instructions, sequence)
+part_1 = ''.join(chr(c) for c in sequence)
+
+print_solutions(part_1)
+# Part 1 solution is: kgdchlfniambejop
