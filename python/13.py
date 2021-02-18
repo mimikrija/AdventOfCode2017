02da7d3f284b9185ab7163c4eed651b4de125393
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-18 12:14:22 +0100

    [day13] use generator and count() instead of while loop
    note: this could be used with itertools.islice in case we needed a list of more such
    delays

--- python/13.py
+++ python/13.py
@@ -1,6 +1,7 @@
 # Day 13: Packet Scanners
 
 from santas_little_helpers import *
+from itertools import count
 
 def scanner_at_zero_position(time, size):
     return time % ((size-1)*2) == 0
@@ -12,12 +13,10 @@ depth_and_range = {layer: size for layer, size in (map(int, line.split(': ')) fo
 severity = sum(time*size for time, size in depth_and_range.items() if scanner_at_zero_position(time, size))
 
 # part 2: find initial delay which makes sure we don't get caught
-delay = 0
-while True:
-    if not any(scanner_at_zero_position(time+delay, size) for time, size in depth_and_range.items()):
-        break
-    else:
-        delay += 1
+delay = next(offset for offset in count() # count() generates an endless range
+            if not any(scanner_at_zero_position(time+offset, size)
+            for time, size in depth_and_range.items()))
+
 
 print_solutions(severity, delay)
 # Part 1 solution is: 2384
