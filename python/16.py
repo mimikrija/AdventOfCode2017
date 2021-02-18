73c54c3b03608a3c35795c90892fafb2fb1b0989
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-18 20:24:12 +0100

    [day16] nailed the repeating dance patterns!
    no need to run the simulation for one billion dances. I figured this out
    by running one thousand dances and putting the resulting strings in a
    set. the length of the resulting set was 42, which means that there are
    most probably only 42 unique dances in the simulation which repeat
    periodically. I tried that and it worked!

--- python/16.py
+++ python/16.py
@@ -13,6 +13,7 @@ def exchange(in_list, A, B):
 def partner(in_list, name_A, name_B):
     return exchange(in_list, in_list.index(ord(name_A)), in_list.index(ord(name_B)))
 
+
 def dance(in_instructions, sequence):
     for args in in_instructions:
         try:
@@ -26,12 +27,31 @@ def dance(in_instructions, sequence):
     return sequence
 
 
-dance_instructions = (instruction[1:].split('/') for instruction in get_input('inputs/16',','))
-sequence = list(range(ord('a'), ord('p')+1))
+def first_42_dances(in_instructions, in_sequence):
+    # after running about 1000 dances and putting them into a set,
+    # I realized that there are only 42 unique dance solutions which
+    # start repeating - it is enough to generate first 43 - index 0
+    # denotes the starting position before any dance is made
+    sequence = list(in_sequence)
+    dances = [sequence]
+    for _ in range(43):
+        sequence = dance(list(dance_instructions), sequence)
+        dances.append(sequence)
+    return dances
 
 
-sequence = dance(dance_instructions, sequence)
-part_1 = ''.join(chr(c) for c in sequence)
+def convert_list_to_string(in_list):
+    return ''.join(chr(c) for c in in_list)
 
-print_solutions(part_1)
+
+dance_instructions = [instruction[1:].split('/') for instruction in get_input('inputs/16',',')]
+start_sequence = list(range(ord('a'), ord('p')+1))
+
+
+first_dance = convert_list_to_string(dance(dance_instructions, start_sequence))
+# there is no need to dance a billion times because the dances start repeating after 42 times
+billionth_dance = convert_list_to_string(first_42_dances(dance_instructions, start_sequence)[(1000*1000*1000)%42])
+
+print_solutions(first_dance, billionth_dance)
 # Part 1 solution is: kgdchlfniambejop
+# Part 2 solution is: fjpmholcibdgeakn
