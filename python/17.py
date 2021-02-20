50afc58e878c898cea2886ed36534f5b1c47faf3
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-20 11:41:37 +0100

    [day17] figure out insertion formula for part 2

--- python/17.py
+++ python/17.py
@@ -1,6 +1,12 @@
 # Day 17: Spinlock
 from santas_little_helpers import print_solutions
 
+def insert_position(current_position, step, size):
+    if current_position + step >= size:
+        return (current_position + step) % size + 1
+    else:
+        return current_position + step + 1
+
 step = 386
 #step = 3
 
@@ -8,7 +14,7 @@ step = 386
 circle = {0: 0}
 last = 0
 
-for pos in range(2017+1):
+for pos in range(1,2017+1):
     # insert element into a linked list
     last_points_to = circle[last]
     circle[last] = pos
@@ -19,7 +25,14 @@ for pos in range(2017+1):
         last_points_to = circle[last]
         last = last_points_to
 
+pos = 0
+for size in range(1, 50000000):
+    pos = insert_position(pos, step, size)
+    if pos == 1:
+        part_2 = size
+
 
 part_1 = circle[2017]
-print_solutions(part_1)
+print_solutions(part_1, part_2)
 # Part 1 solution is: 419
+# Part 2 solution is: 46038988
