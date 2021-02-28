60ec68dad1edaaa3efdf3baac8bc7a0cd06d0ab9
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-28 11:52:08 +0100

    [day23] assembly translation lines 12-20 to a while loop

--- python/23-pt2.py
+++ python/23-pt2.py
@@ -9,17 +9,19 @@ d = 2
 # lines above here
 
 e = 2  # 11
-
-g = d  # 12
-g *= e
-g -= b
-if g == 0: # if g != 0 goto 17 (if we invert the condition we get this)
-    f = 0
-e += 1 # 17
-g = e
-g -= b # 19
-print(g != 0)
-# if g != 0 go to 12
+g = d  # 12 (I'll just leave this here because this is another one we jump to)
+while True:
+    g = d   # 12
+    g *= e
+    g -= b
+    if g == 0: # if g != 0 goto 17 (if we invert the condition we get this)
+        f = 0
+    e += 1 # 17
+    g = e
+    g -= b # 19
+    if g == 0: # again, I invert the condition, I think this jnz translates to
+        break  # a while loop like this
+    # if g != 0 go to 12
 
 
 print(b, c, d, e, f, g, h)
