45974ac2e122de8283cf13302f7beb44ab236293
Maja Gacesa <maja.gacesa@gmail.com>
2021-02-28 19:55:39 +0100

    [day24] define bridge strength function

--- python/24.py
+++ python/24.py
@@ -2,6 +2,11 @@
 
 from santas_little_helpers import *
 
+
+def bridge_strength(in_bridge):
+    return sum(sum(comp) for comp in in_bridge)
+
+
 components = sorted([tuple(sorted(map(int, line.split('/')))) for line in get_input('inputs/24')])
 
 # i did this by looking at the input, there's only
