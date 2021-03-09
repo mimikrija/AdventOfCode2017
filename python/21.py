81575f281ae1d462eb2f4ae9b39d9a24016aeff9
Maja Gacesa <maja.gacesa@gmail.com>
2021-03-09 18:39:03 +0100

    [day21] IDK

--- python/21.py
+++ python/21.py
@@ -1,6 +1,7 @@
 # Day 21: Fractal Art
 
 from santas_little_helpers import *
+from itertools import chain
 
 def all_configurations_of(in_rule):
 
@@ -8,19 +9,18 @@ def all_configurations_of(in_rule):
         rows = in_rule.split('/')
         return '/'.join(rows[::-1])
 
-    def flip_both(in_rule):
-        rows = [row[::-1] for row in flip_horizontal(in_rule).split('/')]
+    def transpose(in_rule): # this should be a transpose
+        rows = [''.join(c for c in trans) for trans in zip(*in_rule.split('/'))]
         return '/'.join(rows)
 
     config = str(in_rule)
     configs = {config}
 
     for _ in range(4):
-        config = flip_both(config)
+        config = transpose(config)
         configs.add(config)
         config = flip_horizontal(config)
         configs.add(config)
-
     return configs
 
 
@@ -34,15 +34,70 @@ def expand_rules(in_rules):
     return rules
 
 
-def translate_to_set(rule):
+def translate_rule_to_set(rule):
     "translates string to a set of local coordinates of that block"
     return {(row, column) for row, line in enumerate(rule.split('/'))
             for column, c in enumerate(line)
             if c == '#'}
 
 
+def translate_set_to_rule(coords, size):
+    out_rule = ''
+    for row in range(size):
+        line = ['.']*size
+        for column in range(size):
+            if (row, column) in coords:
+                line[column] = '#'
+        out_rule += ''.join(line) + '/'
+    return out_rule[:-1]
 
-raw_rules = (line.split(' => ') for line in get_input('inputs/21'))
+
+def local_to_global(in_matrix, sub_block_size):
+    size = len(in_matrix)
+    out_global = set()
+    for x in range(size):
+        for y in range(size):
+            out_global.add
+
+
+def global_to_local(in_global, sub_block_size, blocks):
+    max_hor = max(in_global)[0]
+    max_vert = max(in_global,key=lamda x: x[1])[0]
+    out_matrix = []
+    for row in blocks:
+        for column in blocks:
+            out_matrix.append({(x, y) for (x, y) in in_global 
+            if column <= x < column + sub_block_size and row <= y < row + sub_block_size})
+    return out_matrix
+
+def enhance(in_matrix, sub_block_size):
+    blocks_hor = len(in_matrix)
+    block_vert = len(in_matrix[0])
+    block_size = len(in_matrix[0][0].split('/')[0])
+    size = blocks_hor * sub_block_size
+    if block_size == 3:
+        # enhance blocks immediatelly
+        out_matrix = []
+        for row, row_block in enumerate(in_matrix):
+            new_line = [' ']*blocks_hor
+            for column, block in enumerate(row_block):
+                new_line[column] = expanded_rules[block]
+            out_matrix.append(new_line)
+        enhanced_block_size = 4
+
+
+    else:
+        divisions = next(div for div, rest in (divmod(size, n) for n in {2,3}) if rest == 0)
+
+
+    
+    print(block_vert, blocks_hor, size)
+    return out_matrix, enhanced_block_size
+
+
+
+
+raw_rules = (line.split(' => ') for line in get_input('inputs/21-ex'))
 
 # a basic set of rules; from input
 rules = {left: translate_to_set(right) for left, right in raw_rules}
@@ -50,3 +105,10 @@ rules = {left: translate_to_set(right) for left, right in raw_rules}
 # an expanded set of rules, takes into acc. all configurations
 expanded_rules = expand_rules(rules)
 
+#print(list(expanded_rules.keys()))
+
+start = [['.#./..#/###']]
+
+#print(all_configurations_of('.#./..#/#..'))
+
+print(enhance(start, 3))
